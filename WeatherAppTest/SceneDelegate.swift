298a41c34e824f56c9caabaa1ff840db3f031dac
Mikita <nikita_popow_1993@mail.ru>
2023-02-27 12:52:23 +0300

    start project

--- WeatherAppTest/SceneDelegate.swift
+++ WeatherAppTest/SceneDelegate.swift
@@ -13,10 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let winSc = (scene as? UIWindowScene) else { return }
+        var window = UIWindow(windowScene: winSc)
+        let navController = UINavigationController()
+        let vc = SelecCityAndInfoViewController()
+        navController.viewControllers = [vc]
+        window.rootViewController = navController
+        self.window = window
+        window.makeKeyAndVisible()
+        window.backgroundColor = .white
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
